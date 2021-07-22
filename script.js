8ee4a8edabde6c0a62420930e66b2f620e63ca5a
your_username <kironpaul889@gmail.com>
2021-07-22 21:29:48 +0530

    First way to fecth the data from serve using axios

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+/**
+* @FirstMethod
+*/
 
-axios.get('https://api.funtranslations.com/translate/pirate.json?text=Hello sir').then(res => console.log(res))
+document.getElementById("btn").addEventListener("click", () => {
+    let textValue = document.getElementById("text").value;
+    axios.get('https://api.funtranslations.com/translate/pirate.json?text=' + textValue)
+        .then(res => renderTextDiv(res.data.contents.translated));
+})
+
+
+function renderTextDiv(text) {
+    document.getElementById("text-converted").innerText = text;
+}
+
+
