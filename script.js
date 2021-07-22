defe57a50167fa67b552378a9e46b03e71dbbf60
your_username <kironpaul889@gmail.com>
2021-07-22 22:22:22 +0530

    Add second method

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 * @FirstMethod
 */
 
-document.getElementById("btn").addEventListener("click", () => {
+/* document.getElementById("btn").addEventListener("click", () => {
     let textValue = document.getElementById("text").value;
     axios.get('https://api.funtranslations.com/translate/pirate.json?text=' + textValue)
         .then(res => renderTextDiv(res.data.contents.translated));
@@ -11,6 +11,38 @@ document.getElementById("btn").addEventListener("click", () => {
 
 function renderTextDiv(text) {
     document.getElementById("text-converted").innerText = text;
+} */
+
+
+/** 
+ * @SecondMethod
+ * @Without @catchcall @catchERROR
+ */
+
+const url = 'https://api.funtranslations.com/translate/pirate.json?text='
+document.getElementById("btn").addEventListener("click", () => {
+    let textValue = document.getElementById("text").value;
+    fetch(getTextUrl(textValue))
+        .then(e => e.json())
+        // .then(v => displaytext(v.contents.translated))
+        .then(v => displaytext(v))
+    // .catch(e => displaytext(e))
+
+});
+
+function getTextUrl(textUrl) {
+    return url + textUrl;
 }
 
-
+function displaytext(textDisplay) {
+    console.log(textDisplay)
+    if (textDisplay.error) {
+        const divBody = document.getElementById("text-converted");
+        divBody.innerText = 'Wait Dude ! 🤍'
+        divBody.style.backgroundColor = 'red'
+    } else {
+        const divBody = document.getElementById("text-converted");
+        divBody.innerText = textDisplay.contents.translated;
+        divBody.style.backgroundColor = 'yellow'
+    }
+}
